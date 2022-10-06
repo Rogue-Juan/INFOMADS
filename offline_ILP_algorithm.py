7fa588753590b541f05030d3be52a3ddea0eea81
Rogue-Juan <91721131+Rogue-Juan@users.noreply.github.com>
2022-10-06 12:50:42 +0200

    Update offline_ILP_algorithm.py

--- offline_ILP_algorithm.py
+++ offline_ILP_algorithm.py
@@ -2,6 +2,7 @@
 
 from array import array
 from logging import exception
+from turtle import pen
 import scipy
 import numpy as np
 from scipy import optimize
@@ -22,25 +23,47 @@ def solve_ilp(image_sizes,
     for image in range(len(penalty_scores)):
         for block in range(len(penalty_scores[0])):
             penalty_scores[image,block] = image_sizes[image] * (block+1)
-    print(penalty_scores)
+    penalty_scores = penalty_scores.flatten()
+   # print(penalty_scores)
     
     bounds = optimize.Bounds(0,1)
-    integrality = np.full_like(image_sizes, True)
+    integrality = np.full_like(penalty_scores, True)
     
     bigA = np.ndarray( shape = (len(block_capacities), len(image_sizes)) )
     for i in range(len(block_capacities)):
         bigA[i] = image_sizes
 
-    #print(bigA)
-    constraints = optimize.LinearConstraint(A = bigA, lb = 0, ub = block_capacities)
+    flat_A = bigA.flatten('F')  # 'F' represents column-major ordering
+
+    bigA = np.full_like(a = np.ndarray(shape=(len(block_capacities), len(flat_A)))
+                       , fill_value=0)
+    for i in range(len(block_capacities)):
+        for j in range(len(flat_A)):
+            if j % len(block_capacities) == i:
+                bigA[i][j] = flat_A[j]
+
+#    print(bigA)
+
+    constraint1 = optimize.LinearConstraint(A = bigA, lb = 0, ub = block_capacities)
+
+    bigB = np.zeros((len(image_sizes),len(block_capacities)*len(image_sizes)))
+
+    count = 0
+    for j in range(len(block_capacities)*len(image_sizes)):
+        bigB[count][j] = 1
+        if (j+1) % len(block_capacities) == 0:
+            count += 1
+
+    #print(bigB)
+    constraint2 = optimize.LinearConstraint(A = bigB, lb = 1, ub = 1)
     
-    res = scipy.optimize.milp(c = -image_sizes
-                             , constraints = constraints
+    res = scipy.optimize.milp(c = penalty_scores
+                             , constraints = [constraint1,constraint2]
                              , integrality = integrality
                              , bounds = bounds)
     
     return res
 
-res = solve_ilp([5,3,4], [20,5])
+res = solve_ilp([5,3,4], [5,5,5])
 print("Result is:\n",res.x)
     
