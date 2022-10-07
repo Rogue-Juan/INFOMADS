c9bb541866099da00975cc5f5d8c353a7b1e87eb
Rogue-Juan <91721131+Rogue-Juan@users.noreply.github.com>
2022-10-07 20:22:44 +0200

    Update offline_framework.py

--- offline_framework.py
+++ offline_framework.py
@@ -1,13 +1,14 @@
 # this is the file where we input the situation and get the output solution
 
+from decimal import ROUND_UP
 import numpy as np
 from offline_ILP_algorithm import solve_ilp
 
 
 
 # acquire data from txt file
-
-with open('testInstance1.txt') as f:
+filename = 'testInstance1'
+with open(filename+".txt") as f:
     try:
         number_of_images = int(f.readline())
     except:
@@ -41,18 +42,18 @@ with open('testInstance1.txt') as f:
             length = float(length)
             if length == float('inf'):
                 infinite_interruption = True
-                print('infinite interruption present')
+                # print('infinite interruption present')
                 length = np.Inf
         except:
             raise ValueError("Wrongful input for interruption length")
         finally:
             interruptions.append((start_time,length))
     
-    print("images:",images)
-    print("interruptions:",interruptions)
+    # print("images:",images)
+    # print("interruptions:",interruptions)
     
     interruptions = sorted(interruptions, key=lambda x: x[0])
-    print("interruptions:",interruptions)
+    # print("interruptions:",interruptions)
 
 
     if infinite_interruption == True:
@@ -64,6 +65,8 @@ with open('testInstance1.txt') as f:
 # calculate capacity of each block
 
     blocks = np.zeros(number_of_blocks)
+    blockStarts = np.zeros(number_of_blocks)
+    blockStarts[0] = 0
     blockstart = 0
     
     assert len(interruptions) == number_of_interruptions
@@ -72,19 +75,27 @@ with open('testInstance1.txt') as f:
         if i < number_of_interruptions:
             blocks[i] = interruptions[i][0] - blockstart
             blockstart = interruptions[i][0] + interruptions[i][1]
+            blockStarts[i+1] = blockstart
         else:
             blocks[i] = np.Inf
             
-    print("block capacities:")
-    print(blocks)
+    # print("block capacities:")
+    # print(blocks)
             
 # blocks = [capacity1, capacity2, capacity3]; index is the block number, length is total number of blocks
     
-        
-    
-    
+
 solution = solve_ilp(images, blocks)
-print(solution.x)
+
+imageStarts = np.zeros(number_of_images)
+
+for i in range(number_of_images*number_of_blocks):
+    if solution.x[i] == 1:
+        whichImage = np.floor(i / number_of_blocks) # index starts from 0
+        whichBlock = i % number_of_blocks # in case of 6 blocks: goes from 0 to 5
+        imageStarts[whichImage] = blockStarts[whichBlock]
+        blockStarts[whichBlock] += images[whichImage]
+
     
     
     
